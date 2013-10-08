0347c6378501a6cf7c6018c9491223ce18bd4675
Thomas Herve <th@rackspace.com>
2013-10-08 16:53:45 -0700

    Add a periodic job

---
+++ ersid/dump.py
@@ -0,0 +1,14 @@
+import json
+
+from twisted.internet.task import LoopingCall
+
+
+def backup(service, filename):
+    out = file(filename, 'w')
+    out.write(json.dumps(service.storage))
+    out.close()
+
+
+def startLoop(interval, service, filename):
+    loop = LoopingCall(backup, service, filename)
+    loop.start(interval)
